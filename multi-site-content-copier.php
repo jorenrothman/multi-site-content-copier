98c019d230dfe218d812768791c1f171d6193eb6
Joren Rothman <rothmanjoren@gmail.com>
2020-04-07 11:25:02 +0200

    Bumped Version to 1.2

--- multi-site-content-copier.php
+++ multi-site-content-copier.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Multi Site Content Copier
  * Plugin URI:        https://instance.studio
  * Description:       A Multi Site Content duplication plugin.
- * Version:           1.0.0
+ * Version:           1.2.0
  * Author:            Joren Rothman
  * Author URI:        https://instance.studio
  * License:           GPL-2.0+
